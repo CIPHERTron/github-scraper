806b940fb581b9f853bb79a36a3a740e8080f634
CIPHERTron <pritish.samal918@gmail.com>
2022-02-02 15:21:35 +0530

    feat: setup go-github client & display repo data

--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/CIPHERTron/github-scraper
 go 1.17
 
 require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-github/v42 v42.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+)
+
+type Details struct {
+	Name        string
+	Description string
+	Stars       int
+	Forks       int
+	License     github.License
+}
 
 func main() {
-	fmt.Println("Welcome to the GitHub Scraper")
+	// Loading .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading the .env file")
+	}
+
+	var accessKey string = os.Getenv("ACCESS_TOKEN")
+
+	ctx := context.Background()
+	tokenService := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessKey},
+	)
+
+	tokenClient := oauth2.NewClient(ctx, tokenService)
+
+	// go-github client
+	client := github.NewClient(tokenClient)
+
+	repo, _, err := client.Repositories.Get(ctx, "CIPHERTron", "kaagaz")
+
+	if err != nil {
+		log.Fatalf("Error getting organizations list")
+	}
+
+	data := &Details{
+		Name:        *repo.FullName,
+		Description: *repo.Description,
+		Stars:       *repo.StargazersCount,
+		Forks:       *repo.ForksCount,
+		License:     *repo.GetLicense(),
+	}
+
+	dataJSON, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println(string(dataJSON))
+
 }
